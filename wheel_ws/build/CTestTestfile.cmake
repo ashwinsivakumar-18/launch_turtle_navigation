3998ba2a138d63ad13122450035fe8f182af1bf6
shashiraj <shashiraj.ps@ssautomation.in>
2025-06-03 13:00:31 +0530

    Initial

---
+++ wheel_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shashiraj/wheel_ws/src
+# Build directory: /home/shashiraj/wheel_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("two_wheel_robot")
---
+++ wheel_ws/build/two_wheel_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shashiraj/wheel_ws/src/two_wheel_robot
+# Build directory: /home/shashiraj/wheel_ws/build/two_wheel_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
