dcb5f773289a3df3c5b9aa851fcbcec6776cf85c
ashwinsivakumar-18 <ashu.ashwin.18082003@gmail.com.com>
2025-06-11 12:14:02 +0530

    Ros1 to Ros2

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shashiraj/2wheel_drive/src
-# Build directory: /home/shashiraj/2wheel_drive/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("two_wheel_robot")
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/shashiraj/2wheel_drive/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
